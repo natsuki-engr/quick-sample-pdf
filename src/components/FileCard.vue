9ff701e6267273979200fef6fb0d637c88373dd6
natsuki-engr <biafrajr@gmail.com>
2024-06-09 10:31:13 +0900

    update style of FileCard

--- src/components/FileCard.vue
+++ src/components/FileCard.vue
@@ -21,20 +21,25 @@ const { clickRemoveBtn } = setup(emits)
 </script>
 
 <template>
-  <div class="group relative mb-5 max-w-5xl cursor-default rounded-xl border-2 border-solid border-violet-500 bg-white p-4">
+  <div class="group relative mb-5 flex max-w-5xl cursor-default flex-col rounded-xl border-2 border-solid border-violet-500 bg-white px-3 py-4">
     <button
       class="invisible absolute right-0 top-0 h-9 w-9 rounded-bl-md rounded-tr-lg bg-violet-500 group-hover:visible"
       @click="clickRemoveBtn"
     >
       <i class="i-bx-x absolute inset-0 m-auto bg-white text-3xl"></i>
     </button>
-    <span
-      class="block max-w-2xl overflow-hidden text-ellipsis whitespace-nowrap text-xl"
-      v-text="file.fileName"
-    ></span>
+
+    <div class="flex items-center">
+      <i class="i-bxs-file-pdf text-3xl text-violet-500"></i>
+      <span
+        class=" max-w-2xl overflow-hidden text-ellipsis whitespace-nowrap text-xl"
+        v-text="file.fileName"
+      ></span>
+    </div>
+
     <div class="flex items-center">
       <span
-        class="text-violet-400"
+        class="ps-2 text-violet-400"
         v-text="file.dir"
       ></span>
       <span
@@ -44,5 +49,3 @@ const { clickRemoveBtn } = setup(emits)
     </div>
   </div>
 </template>
-
-<style scoped></style>
